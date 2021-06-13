966bb5d97a345f6cc79a0a2c1f15cc3e59ef11d3
José Carlos Soares de Souza <josecarlos@globtec.com.br>
2021-06-13 11:29:43 -0300

    Start the Street Fair project generated with spring initializr

---
+++ src/main/java/io/unico/streetfair/StreetfairApplication.java
@@ -0,0 +1,13 @@
+package io.unico.streetfair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetfairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetfairApplication.class, args);
+	}
+
+}
