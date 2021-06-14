b51eb34a71c6a66231521a9f109806afc1b81dac
José Carlos Soares de Souza <josecarlos@globtec.com.br>
2021-06-13 23:46:00 -0300

    Add the fair entity

---
+++ src/main/java/io/unico/streetfair/domain/entity/Fair.java
@@ -0,0 +1,44 @@
+package io.unico.streetfair.domain.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Fair {
+
+    private String id;
+
+    private String lat;
+
+    private String lon;
+
+    private String setcems;
+
+    private String weightingArea;
+
+    private String districtCode;
+
+    private String district;
+
+    private String subprefectureCode;
+
+    private String subprefecture;
+
+    private String region5;
+
+    private String region8;
+
+    private String fair_name;
+
+    @Id
+    private String registry;
+
+    private String address;
+
+    private String number;
+
+    private String neighborhoods;
+
+    private String landmark;
+
+}
